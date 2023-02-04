79334910f816db5f240a0c73e624b408de29916a
Cesar Lopez <cesarlerdeut@gmail.com>
2023-02-04 22:53:38 +0100

    Setup logger

--- KafkaNet/Program.cs
+++ KafkaNet/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = Host.CreateDefaultBuilder(args).Build();
+
 Console.WriteLine("Hello, World!");
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Host created!");
+
+await host.RunAsync();
